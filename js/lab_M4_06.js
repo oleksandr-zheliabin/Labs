ff974825735b04700874e4820c083e8f987a154e
Oleksandr Zheliabin <oleksandr.zheliabin@gmail.com>
2023-10-11 17:04:50 -0700

    Initial commit

---
+++ js/lab_M4_06.js
@@ -0,0 +1,26 @@
+// GLOBAL VARIABLES
+let futureValue;
+let investment;
+let rate;
+let years;
+
+// COLLECT VALUES FROM THE USER
+
+investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
+console.log(typeof(investment))
+
+
+rate = parseFloat(prompt('Enter interest rate as xx.x'));
+years = parseInt(prompt('Enter the number of years you want to invest for'));
+
+// CALCULATE FUTURE VALUE
+futureValue = investment;
+for (let i = 0; i < years; i++) {
+    futureValue = futureValue + (futureValue * rate / 100);
+}
+
+// DISPLAY RESULT
+document.write(`Investment amount: $${investment.toFixed(2)}<br>`);
+document.write(`Interest rate: ${rate.toFixed(2)}%<br>`);
+document.write(`Years: ${years}<br>`);
+document.write(`Future value: $${futureValue.toFixed(2)}`);
